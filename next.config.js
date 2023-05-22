85b457fa5db847f3a03c87a37c6f0bf772ffd82c
Narges <nargeskhatami.h@gmail.com>
2023-05-23 00:58:13 +0330

    add github avatar url support

--- next.config.js
+++ next.config.js
@@ -1,6 +1,14 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
-}
-
-module.exports = nextConfig
+  images: {
+    remotePatterns: [
+      {
+        protocol: "https",
+        hostname: "avatars.githubusercontent.com",
+        port: "",
+        pathname: "/u/**",
+      },
+    ],
+  },
+};

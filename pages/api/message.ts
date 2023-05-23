598a5f0b1f0b678fd2c2e2acd7c1f179fe573d4a
Narges <nargeskhatami.h@gmail.com>
2023-05-24 01:44:10 +0330

    add get protocol to messages api

--- pages/api/message.ts
+++ pages/api/message.ts
@@ -12,10 +12,28 @@ export default async function handler(req, res) {
           receiverId,
         },
       });
-      console.log('res', res);
-      
       res.status(201).json(post);
       break;
+    case "GET":
+      const { senderId: sId, receiverId: rId } = req.query;
+      const messages = await prisma.message.findMany({
+        where: {
+          OR: [
+            {
+              senderId: sId,
+              receiverId: rId,
+            },
+            {
+              senderId: rId,
+              receiverId: sId,
+            },
+          ],
+        },
+      });
+      messages.sort((a, b) => {
+        return new Date(a.createdAt).getTime() - new Date(b.createdAt).getTime();
+      });
+      res.status(200).json(messages);
     default:
       res.setHeader("Allow", ["POST"]);
       res.status(405).end(`Method ${method} Not Allowed`);

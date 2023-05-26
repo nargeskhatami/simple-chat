210dcef1a8eea4183ef0b6a9a3e9a13d9058b7a7
Narges <nargeskhatami.h@gmail.com>
2023-05-27 01:58:25 +0330

    add message api route that creates & gets user messages

--- pages/api/message.ts
+++ pages/api/message.ts
@@ -1,10 +1,12 @@
 import { prisma } from "@/server/db/client";
+import type { NextApiRequest, NextApiResponse } from "next";
 
-export default async function handler(req, res) {
+export default async function handler(req: NextApiRequest, res: NextApiResponse) {
   const { method } = req;
   switch (method) {
     case "POST":
       const { text, senderId, receiverId } = req.body;
+      // create a new message
       const post = await prisma.message.create({
         data: {
           text,
@@ -15,25 +17,38 @@ export default async function handler(req, res) {
       res.status(201).json(post);
       break;
     case "GET":
-      const { senderId: sId, receiverId: rId } = req.query;
-      const messages = await prisma.message.findMany({
+      const { senderI: sId, receiverId: rId } = req.query;
+      // find all messages between two users
+      let messages = await prisma.message.findMany({
         where: {
           OR: [
             {
-              senderId: sId,
-              receiverId: rId,
+              senderId: sId as string,
+              receiverId: rId as string,
             },
             {
-              senderId: rId,
-              receiverId: sId,
+              senderId: rId as string,
+              receiverId: sId as string,
             },
           ],
         },
       });
       messages.sort((a, b) => {
-        return new Date(a.createdAt).getTime() - new Date(b.createdAt).getTime();
+        return new Date(b.createdAt).getTime() - new Date(a.createdAt).getTime();
       });
-      res.status(200).json(messages);
+      const result = messages.map((message) => {
+        return {
+          ...message,
+          // convert date format to 'HH:mm' format
+          createdAt: new Intl.DateTimeFormat("en-US", {
+            hour: "2-digit",
+            minute: "2-digit",
+            hour12: false,
+          }).format(new Date(message.createdAt)),
+        };
+      });
+      res.status(200).json(result);
+      break;
     default:
       res.setHeader("Allow", ["POST"]);
       res.status(405).end(`Method ${method} Not Allowed`);

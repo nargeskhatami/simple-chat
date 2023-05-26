a552e5e353fd68c84d711c21b222af1ff01bdc8c
Narges <nargeskhatami.h@gmail.com>
2023-05-27 01:57:43 +0330

    add chat components to the home page

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,86 +1,74 @@
 import { getServerSession } from "next-auth";
-import { useSession, signIn, signOut } from "next-auth/react";
+import { useSession } from "next-auth/react";
+import { DefaultUser, Session } from "next-auth/core/types";
 import { authOptions } from "./api/auth/[...nextauth]";
 import { GetServerSidePropsContext } from "next/types";
-import Image from "next/image";
 
-import ChatRoom from "@/components/ChatRoom";
-import { prisma } from "@/server/db/client";
+import UserChatRoom from "@/components/UserChatRoom";
+import UsersList from "@/components/UsersList";
+
 import { useState } from "react";
 
-export default function Home({ users }) {
-  const { data: session } = useSession();
-  const [receiver, setReceiver] = useState(null);
-  if (session) {
-    return (
-      <>
-        <div className="flex items-center justify-end fixed top-0 w-screen right-0">
-          Signed in as {session.user?.email} <br />
-          <button
-            className="bg-orange-600 py-2 px-6 m-3 rounded text-white"
-            onClick={() => signOut()}
-          >
-            Sign out
-          </button>
-        </div>
+import { User } from "@/types/User";
+import { prisma } from "@/server/db/client";
 
-        <div className="h-screen w-screen flex items-center justify-center">
-          {receiver ? <ChatRoom sender={session.user} receiver={receiver} /> : "Nothing to show"}
-        </div>
-        <aside className="fixed left-0 top-0 h-screen border-r border-grey">
-          <ul className="divide-y">
-            {users.map((user) => (
-              <li
-                key={user.id}
-                className="transition-all flex items-center p-4 cursor-pointer hover:bg-gray-200"
-                onClick={() => {
-                  setReceiver(user);
-                }}
-              >
-                <Image
-                  className="rounded-full"
-                  unoptimized
-                  src={user.image || "/images/Defalut-Avatar.jpg"}
-                  width={50}
-                  height={50}
-                  alt={user.name}
-                />
-                <h2 className="px-2">{user.name}</h2>
-              </li>
-            ))}
-          </ul>
-        </aside>
-      </>
-    );
-  }
+export default function Home({ users }: { users: User[] }) {
+  const { data: session } = useSession() as { data: Session };
+  const [receiver, setReceiver] = useState<User | null>(null);
+
   return (
-    <div className="m-auto flex flex-col items-center justify-center">
-      Not signed in <br />
-      <button className="bg-orange-600 py-2 px-6 m-3 rounded text-white" onClick={() => signIn()}>
-        Sign in
-      </button>
-    </div>
+    <>
+      <UsersList users={users} setReceiver={(user: User) => setReceiver(user)} />
+      <div className="h-screen flex items-center justify-center w-[calc(100%-250px)]">
+        {receiver ? (
+          <UserChatRoom
+            sender={
+              session.user as DefaultUser & {
+                id: string;
+              }
+            }
+            receiver={receiver}
+          />
+        ) : (
+          <span className="bg-zinc-400/25 py-2 px-6 rounded-full text-zinc-200 ">
+            Select a chat to start messaging
+          </span>
+        )}
+      </div>
+    </>
   );
 }
 
 export async function getServerSideProps(context: GetServerSidePropsContext) {
-  const session = await getServerSession(context.req, context.res, authOptions);
-  let users = [];
+  const session = (await getServerSession(context.req, context.res, authOptions)) as Session;
+  let users: User[] = [];
   if (session) {
     let data = await prisma.user.findMany();
+    let currentUser: User | null = null;
     data.map((user) => {
-      if (user.id !== session.user?.id) users.push(user);
+      if (user.id !== session.user?.id)
+        users.push({
+          ...user,
+          isCurrentUser: false,
+        });
+      else
+        currentUser = {
+          ...user,
+          isCurrentUser: true,
+        };
     });
+    if (currentUser) {
+      users.push(currentUser);
+      users.reverse();
+    }
   } else {
     return {
       redirect: {
-        destination: "/api/auth/signin?callbackUrl=http://localhost:3000",
+        destination: "/auth/signin",
         permanent: false,
       },
     };
   }
-  console.log(users);
-  
   return {
     props: {
       session,
